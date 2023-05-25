1add6e2fd140941366b4ebe00b077e914875ec66
Anmoljagota <anmoljagota08@gmail.com>
2023-05-25 21:09:49 +0530

    Routes set

---
+++ src/AllRouutes/AllRoutes.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Home from "../Pages/Home";
+const AllRoutes = () => {
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+      </Routes>
+    </div>
+  );
+};
+
+export default AllRoutes;
